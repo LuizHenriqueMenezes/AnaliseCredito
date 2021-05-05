034dac0b755664109889bdcf594eb3010e80811b
Diego Neri <diego@diegoneri.com>
2021-05-05 08:47:31 -0300

    Corrige lógica do programa
    Parcela não ultrapasse 30% da renda (desconsidere os juros).

--- Program.cs
+++ Program.cs
@@ -6,35 +6,32 @@ namespace AnaliseCredito
     {
         static void Main(string[] args)
         {
-            double valorSolicitado, quantidadeParcelas, rendaMensal;
-            double porcentagemUm, porcentagemDois, porcentagem;
-            
+            double valorSolicitado, quantidadeParcelas, rendaMensal, valorParcela;
+
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("\nOlá cliente, vamos começar a tratar do seu pedido de empréstimo! Para isso, digite: \n");
             Console.ResetColor();
 
             Console.Write("Digite o valor solicitado: ");
             valorSolicitado = Convert.ToDouble(Console.ReadLine());
-            
+
             Console.Write("A quantidade de parcelas desejada: ");
             quantidadeParcelas = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Sua renda mensal: ");
             rendaMensal = Convert.ToDouble(Console.ReadLine());
 
-            porcentagemUm = 30 / 10;
-            porcentagemDois = rendaMensal / 10; 
-            porcentagem = porcentagemUm * porcentagemDois; 
-            
-            if (valorSolicitado > porcentagem)
+            valorParcela = valorSolicitado / quantidadeParcelas;
+
+            if (valorParcela > rendaMensal * 0.3)
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("\nNão é possível fazermos um empréstimo de mais de 30% da rua renda, perdão.\n");
+                Console.WriteLine("\nNão é possível fazermos um empréstimo de mais de 30% da sua renda, perdão.\n");
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine($"\nÉ possível fazermos um empréstimo de {valorSolicitado} reais com {quantidadeParcelas} parcelas. \n");
+                Console.WriteLine($"\nÉ possível fazermos um empréstimo de {valorSolicitado:C} com {quantidadeParcelas} parcelas, sendo {valorParcela:C} cada. \n");
             }
 
             Console.ResetColor();
